a1f8b60206ec1f2829c8fe8b88cc24addfabf68f
ALESSANDRO FUKUTA <alessandro.fukuta@etec.sp.gov.br>
2020-01-04 18:11:07 -0300

    projeto de vetor

---
+++ Vetor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetor {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Quantas Posições:");
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+            double soma = 0;
+            for (int x=0; x < n; x++) {
+
+                Console.WriteLine("Entre com a valor Posição {0} :", x);
+                vect[x] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                soma += vect[x];
+
+            }
+
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Média de Altura: " + (soma/n).ToString("N2",CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
